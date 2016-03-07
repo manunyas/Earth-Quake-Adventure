762f44840dd913132a2d709e4b60ad46c3c90b09
Thonyatorn <dongseng_phone@hotmail.com>
2016-03-08 02:19:43 +0700

    -Edit Main 1 Stage
    -Editing Main 1 Stage
    -Create Main 2 Stage (stage 1)

---
+++ Assets/Script/Player2Control.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Control : MonoBehaviour {
+	public bool grounded = false;
+	public bool shooted = false;
+	public bool hit = false;
+	public bool hitSpecial = false;
+	public Transform groundedPoint,endPoint;
+	public Animator anim;
+	public int jumpCount;
+	Rigidbody2D rd;
+
+	// Use this for initialization
+	void Start () {
+		rd = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RayCasting ();
+	
+	}
+
+	void RayCasting () 
+	{
+		//Debug.DrawLine(this.transform.position, groundedEnd.position,Color.red);
+		Debug.DrawLine(this.transform.position, groundedPoint.position,Color.red);
+		Debug.DrawLine (this.transform.position,endPoint.position,Color.green);
+
+		//Touched the ground
+		grounded = Physics2D.Linecast (this.transform.position, groundedPoint.position, 1 <<LayerMask.NameToLayer("Ground"));
+		//Hit Enermy type obj
+		hit = Physics2D.Linecast (this.transform.position, endPoint.position, 1 << LayerMask.NameToLayer ("Enermy"));
+		//Hit Special Enermy obj or special Obj
+		hitSpecial = Physics2D.Linecast (this.transform.position,endPoint.position, 1<<LayerMask.NameToLayer("EnermySP"));
+		hitSpecial = Physics2D.Linecast (this.transform.position,endPoint.position, 1<<LayerMask.NameToLayer("SpecialLayer"));
+	}
+
+	public void ShootingBool()
+	{
+		anim.SetBool ("isShoot1", true);
+	}
+
+	public void ShootingTrigger()
+	{
+		anim.SetTrigger ("isShoot2");
+	}
+}
