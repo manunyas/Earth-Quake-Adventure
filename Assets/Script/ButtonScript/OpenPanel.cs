8b568c0be59e4e54c9d3cbf8403c4149e702945d
Thonyatorn <dongseng_phone@hotmail.com>
2016-03-10 03:30:16 +0700

    EW 2.2.1
    -Add knowledge while read cartoon chapter1
    -Add Achievement page (Lak Gai ver.)
    -Can choose sub stage in Beach Stage

---
+++ Assets/Script/ButtonScript/OpenPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OpenPanel : MonoBehaviour {
+	public Canvas infoCanvas;
+
+	// Use this for initialization
+	void Start () {
+		infoCanvas = infoCanvas.GetComponent<Canvas> ();
+		infoCanvas.enabled = false;
+	
+	}
+	
+	public void PressOpen()
+	{
+		infoCanvas.enabled = true;
+	}
+
+	public void PressClose()
+	{
+		infoCanvas.enabled = false;
+	}
+}
